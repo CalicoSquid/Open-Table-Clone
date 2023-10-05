864b1c50690f0025d11a6b1c786094a9731d06c9
Caleb Mow <immensedrama@gmail.com>
2023-10-05 13:28:06 +0200

    pp

--- app/page.tsx
+++ app/page.tsx
@@ -13,48 +13,44 @@ export const metadata: Metadata = {
 }
 
 export interface RestaurantCardType {
-  id: number,
-  name: string,
-  main_image: string,
-  cuisine: Cuisine,
-  location: Location,
-  price: PRICE
-  reviews: Review[],
-  slug: string,
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+  reviews: Review[];
 }
 
-const fetchRestaurants = async () => {
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
   const restaurants = await prisma.restaurant.findMany({
     select: {
       id: true,
       name: true,
       main_image: true,
       cuisine: true,
+      slug: true,
       location: true,
       price: true,
       reviews: true,
-      slug: true,
-    }
+    },
   });
 
   return restaurants;
-}
-
+};
 
 export default async function Home() {
-  const restaurants = await fetchRestaurants()
-  const cardArray = restaurants.map(restaurant => {
-    return (
-      <RestaurantCard restaurant={restaurant} />
-    )
-  })
+  const restaurants = await fetchRestaurants();
 
-  return (    
-      <main>
-        <Header />
-        <div className="py-3 px-36 mt-10 flex flex-wrap ">
-          {cardArray}
-        </div>
-      </main>
-  )
+  return (
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
+      </div>
+    </main>
+  );
 }
